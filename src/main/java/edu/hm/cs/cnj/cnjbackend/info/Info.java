a8288d1dccde3aa32a517a0cddcedf458ca6a2bf
Sauer <hm-sauer5@hm.edu>
2017-10-17 14:09:27 +0200

    Klassen angelegt, um laufendes Programm zu erzeugen

---
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/Info.java
@@ -0,0 +1,17 @@
+package edu.hm.cs.cnj.cnjbackend.info;
+
+public class Info {
+	String message;
+
+	public Info() {
+		// Default constructor.
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
