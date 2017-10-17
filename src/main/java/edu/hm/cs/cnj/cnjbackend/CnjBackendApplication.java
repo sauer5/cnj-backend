89b0a5a4d6749134158a316dc47a8c6447da06d9
Sauer <hm-sauer5@hm.edu>
2017-10-17 13:46:01 +0200

    Initiale Erzeugung eines Pring Boot Projektes

---
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplication.java
@@ -0,0 +1,12 @@
+package edu.hm.cs.cnj.cnjbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnjBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.hm.cs.cnj.cnjbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CnjBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
